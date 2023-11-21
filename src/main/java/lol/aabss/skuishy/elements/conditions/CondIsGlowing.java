a71d8b4e5a3a8954db5e9a3e645d830ab79372ae
aabssmc <lilmikeyninja203@gmail.com>
2023-11-21 01:04:32 -0800

    Added IsGlowing condition

---
+++ src/main/java/lol/aabss/skuishy/elements/conditions/CondIsGlowing.java
@@ -0,0 +1,60 @@
+package lol.aabss.skuishy.elements.conditions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Condition;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+
+@Name("Player - Is Glowing")
+@Description("Checks whether a player is glowing.")
+@Examples({
+        "if player is glowing:",
+        "\tmake player stop glowing"
+})
+@Since("1.6")
+
+public class CondIsGlowing extends Condition {
+
+    static{
+        Skript.registerCondition(CondIsGlowing.class,
+                "%players% (is|are) glowing",
+                "%players% (isn't|is not|aren't|are not) glowing"
+        );
+    }
+
+    Expression<Player> player;
+    boolean is;
+
+    @Override
+    public boolean check(@NotNull Event e) {
+        Player p = player.getSingle(e);
+        assert p != null;
+        if (is){
+            return p.isGlowing();
+        }
+        return !p.isGlowing();
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return player.toString(e,debug) + " is glowing";
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        player = (Expression<Player>) exprs[0];
+        is = (matchedPattern == 0);
+        return true;
+    }
+}
