88cdd22851898c0683a00ad860ffbaba5de14240
aabssmc <lilmikeyninja203@gmail.com>
2023-11-30 15:14:27 -0800

    Added Locate Raid Expression

---
+++ src/main/java/lol/aabss/skuishy/elements/expressions/ExprLocateRaid.java
@@ -0,0 +1,67 @@
+package lol.aabss.skuishy.elements.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import org.bukkit.Location;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+
+@Name("World - Nearest Raid")
+@Description("Locates the nearest raid in a specified radius and location.")
+@Examples({
+        "send nearest raid in radius 1000 around player"
+})
+@Since("1.7")
+
+public class ExprLocateRaid extends SimpleExpression<Location> {
+
+    static{
+        Skript.registerExpression(ExprLocateBiome.class, Location.class, ExpressionType.COMBINED,
+                "nearest raid in radius %integer% around %location%"
+        );
+    }
+
+    Expression<Integer> radius;
+    Expression<Location> location;
+
+    @Override
+    protected @Nullable Location[] get(@NotNull Event e) {
+        Location loc = location.getSingle(e);
+        Integer rad = radius.getSingle(e);
+        assert loc != null; assert rad != null;
+        return new Location[]{loc.getWorld().locateNearestRaid(loc, rad).getLocation()};
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public @NotNull Class<? extends Location> getReturnType() {
+        return Location.class;
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "nearest biome";
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        radius = (Expression<Integer>) exprs[0];
+        location = (Expression<Location>) exprs[1];
+        return true;
+    }
+}
