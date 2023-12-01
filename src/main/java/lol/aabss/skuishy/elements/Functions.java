41b6b166e4f0d5cadd841806c1ac6f95956ff6c8
aabssmc <lilmikeyninja203@gmail.com>
2023-11-30 19:34:40 -0800

    added mean function

---
+++ src/main/java/lol/aabss/skuishy/elements/Functions.java
@@ -0,0 +1,33 @@
+package lol.aabss.skuishy.elements;
+
+import ch.njol.skript.lang.function.Parameter;
+import ch.njol.skript.lang.function.SimpleJavaFunction;
+import ch.njol.skript.registrations.DefaultClasses;
+import ch.njol.util.StringUtils;
+import org.jetbrains.annotations.NotNull;
+
+import static ch.njol.skript.lang.function.Functions.registerFunction;
+
+public class Functions {
+    private static String str(double n) {
+        return StringUtils.toString(n, 4);
+    }
+
+    static {
+        Parameter<?>[] numbersParam = new Parameter[]{new Parameter<>("ns", DefaultClasses.NUMBER, false, null)};
+        registerFunction(new SimpleJavaFunction<Number>("mean", numbersParam, DefaultClasses.NUMBER, true) {
+            @Override
+            public Number @NotNull [] executeSimple(Object[] @NotNull [] params) {
+                Object[] ns = params[0];
+                double sum = 0.0;
+                for (Object num : ns) {
+                    sum += ((Number) num).doubleValue();
+                }
+                double mean = sum / ns.length;
+                return new Number[]{mean};
+            }
+        }
+                .description("Calculates the mean of a list of numbers.")
+                .since("1.7"));
+    }
+}
