b129b8758656673ad78eef694a9e3a0e5d05508c
aabssmc <lilmikeyninja203@gmail.com>
2023-12-02 16:46:09 -0800

    all datapacks

---
+++ src/main/java/lol/aabss/skuishy/elements/expressions/ExprAllDatapacks.java
@@ -0,0 +1,54 @@
+package lol.aabss.skuishy.elements.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import io.papermc.paper.datapack.Datapack;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+
+public class ExprAllDatapacks extends SimpleExpression<Datapack> {
+
+    static {
+        Skript.registerExpression(ExprAllDatapacks.class, Datapack.class, ExpressionType.SIMPLE,
+                "[all [of]] [the] [:enabled] datapacks"
+        );
+    }
+
+    private boolean enabled;
+
+    @Override
+    protected @Nullable Datapack[] get(@NotNull Event e) {
+        if (enabled){
+            return Bukkit.getDatapackManager().getEnabledPacks().toArray(new Datapack[0]);
+        }
+        return Bukkit.getDatapackManager().getPacks().toArray(new Datapack[0]);
+    }
+
+    @Override
+    public boolean isSingle() {
+        return false;
+    }
+
+    @Override
+    public @NotNull Class<? extends Datapack> getReturnType() {
+        return Datapack.class;
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "all datapacks";
+    }
+
+    @Override
+    public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        enabled = parseResult.hasTag("enabled");
+        return true;
+    }
+}
