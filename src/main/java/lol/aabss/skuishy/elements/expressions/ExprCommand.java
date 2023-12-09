c2366fc7f325096902697b3fb54787827b4a3fc5
aabssmc <lilmikeyninja203@gmail.com>
2023-12-08 23:58:40 -0800

    added ExprCommand and updated to 1.20.4

---
+++ src/main/java/lol/aabss/skuishy/elements/expressions/ExprCommand.java
@@ -0,0 +1,80 @@
+package lol.aabss.skuishy.elements.expressions;
+
+import ch.njol.skript.classes.Changer;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.expressions.base.PropertyExpression;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import ch.njol.util.coll.CollectionUtils;
+import org.bukkit.block.Block;
+import org.bukkit.block.CommandBlock;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+
+@Name("Block - Command of Command Block")
+@Description("Sends the command in a command block.")
+@Examples({
+        "send command of target block"
+})
+@Since("1.8")
+
+public class ExprCommand extends PropertyExpression<Block, String> {
+
+    static{
+        register(ExprCommand.class, String.class,
+                "command",
+                "block"
+        );
+    }
+
+    @Override
+    protected String @NotNull [] get(@NotNull Event e, Block @NotNull [] source) {
+        if (getExpr().getSingle(e) instanceof CommandBlock c){
+            return new String[]{c.getCommand()};
+        }
+        return new String[0];
+    }
+
+    @Override
+    public void change(@NotNull Event e, Object @NotNull [] delta, Changer.@NotNull ChangeMode mode){
+        if (mode == Changer.ChangeMode.SET) {
+            Block block = getExpr().getSingle(e);
+            if (block instanceof CommandBlock){
+                ((CommandBlock) block).setCommand((String) delta[0]);
+            }
+        } else {
+            assert false;
+        }
+    }
+
+    @Override
+    public Class<?> @NotNull [] acceptChange(final Changer.@NotNull ChangeMode mode) {
+        if (mode == Changer.ChangeMode.SET) {
+            return CollectionUtils.array(String.class);
+        }
+        return CollectionUtils.array();
+    }
+
+    @Override
+    public @NotNull Class<? extends String> getReturnType() {
+        return String.class;
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "command of command block";
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        setExpr((Expression<? extends Block>) exprs[0]);
+        return true;
+    }
+}
