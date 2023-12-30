06a3d66acfd7a5dc2ddf7bcbb60b73af47756618
aabssmc <lilmikeyninja203@gmail.com>
2023-12-30 00:57:33 -0800

    Added EffShowWinScreen

---
+++ src/main/java/lol/aabss/skuishy/elements/effects/EffShowWinScreen.java
@@ -0,0 +1,49 @@
+package lol.aabss.skuishy.elements.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+@Name("Player - Show Win Screen")
+@Description("Shows the win screen to a player.")
+@Examples({
+        "show win screen to event-player"
+})
+@Since("2.0")
+public class EffShowWinScreen extends Effect {
+
+    static{
+        Skript.registerEffect(EffShowWinScreen.class,
+                "show [the] win screen to %player%",
+                "make %player% see the win screen"
+        );
+    }
+
+    private Expression<Player> p;
+
+    @Override
+    protected void execute(@NotNull Event e) {
+        p.getSingle(e).showWinScreen();
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "show win screen";
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        p = (Expression<Player>) exprs[0];
+        return true;
+    }
+}
