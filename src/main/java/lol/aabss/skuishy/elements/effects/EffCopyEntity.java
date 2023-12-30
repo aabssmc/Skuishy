292a2d044b4b91dc5cb65766319f63a37e835196
aabssmc <lilmikeyninja203@gmail.com>
2023-12-30 00:57:23 -0800

    Added EffCopyEntity

---
+++ src/main/java/lol/aabss/skuishy/elements/effects/EffCopyEntity.java
@@ -0,0 +1,66 @@
+package lol.aabss.skuishy.elements.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.Variable;
+import ch.njol.skript.variables.Variables;
+import ch.njol.util.Kleenean;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+
+@Name("Entity - Copy Entity")
+@Description("Makes a copy of an entity.")
+@Examples({
+        "copy event-entity and spawn it at location of event-entity"
+})
+@Since("2.0")
+public class EffCopyEntity extends Effect {
+
+    static {
+        Skript.registerEffect(EffCopyEntity.class,
+                "copy %entity% and (store|put) it in %object%",
+                "copy %entity% and (spawn|put) it at %location%"
+        );
+
+    }
+
+    private Expression<Entity> entity;
+    private Variable<?> var;
+    private Expression<Location> loc;
+
+    @Override
+    protected void execute(@NotNull Event e) {
+        if (loc != null){
+            entity.getSingle(e).copy(loc.getSingle(e));
+        }
+        else{
+            Variables.setVariable(var.getName().toString(), entity.getSingle(e).copy(), e, var.isLocal());
+        }
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "copy entity";
+    }
+
+    @Override
+    public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        entity = (Expression<Entity>) exprs[0];
+        if (matchedPattern == 0){
+            var = (Variable<?>) exprs[1];
+            return true;
+        }
+        loc = (Expression<Location>) exprs[1];
+        return true;
+    }
+}
