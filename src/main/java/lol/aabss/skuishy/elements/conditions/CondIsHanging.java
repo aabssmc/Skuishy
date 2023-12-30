2c1891da99df4f2779717862559ecb8581b4bce4
aabssmc <lilmikeyninja203@gmail.com>
2023-12-30 00:57:02 -0800

    Added CondIsHanging

---
+++ src/main/java/lol/aabss/skuishy/elements/conditions/CondIsHanging.java
@@ -0,0 +1,42 @@
+package lol.aabss.skuishy.elements.conditions;
+
+import ch.njol.skript.conditions.base.PropertyCondition;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import org.bukkit.block.data.Hangable;
+import org.bukkit.entity.Entity;
+import org.jetbrains.annotations.NotNull;
+
+@Name("Entity - Is Hanging")
+@Description("Entity is hanging.")
+@Examples({
+        "if entity is hanging:",
+        "\tbroadcast \"%event-entity% is hanging\""
+})
+@Since("1.6")
+
+public class CondIsHanging extends PropertyCondition<Entity> {
+
+    static{
+        register(CondIsHanging.class,
+                "(hanging|hung)",
+                "entities");
+    }
+
+    @Override
+    public boolean check(Entity entity) {
+        if (entity instanceof Hangable){
+            return ((Hangable) entity).isHanging();
+        }
+        return false;
+    }
+
+    @Override
+    protected @NotNull String getPropertyName() {
+        return "hanging";
+    }
+}
+
+
