a005f2b26e1cb2692aebd7af0d9b8a01f747ea2a
aabssmc <lilmikeyninja203@gmail.com>
2023-12-30 17:29:31 -0800

    Added ExprSensitivity

---
+++ src/main/java/lol/aabss/skuishy/hooks/vulcan/expressions/ExprSensitivity.java
@@ -0,0 +1,51 @@
+package lol.aabss.skuishy.hooks.vulcan.expressions;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.expressions.base.PropertyExpression;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import me.frep.vulcan.api.VulcanAPI;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+@Name("Vulcan - Sensitivity of Player")
+@Description("Represents the sensitivity of a player.")
+@Examples({
+        "send the sensitivity of player"
+})
+@Since("2.0")
+public class ExprSensitivity extends PropertyExpression<Player, Integer> {
+
+    static{
+        register(ExprSensitivity.class, Integer.class,
+                "sens[itivity]",
+                "players");
+    }
+
+    @Override
+    protected Integer @NotNull [] get(@NotNull Event event, Player[] source) {
+        return new Integer[]{VulcanAPI.Factory.getApi().getSensitivity(source[0])};
+    }
+
+    @Override
+    public @NotNull Class<? extends Integer> getReturnType() {
+        return Integer.class;
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "sensitivity of player";
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        setExpr((Expression<? extends Player>) exprs[0]);
+        return true;
+    }
+}
