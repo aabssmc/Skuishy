f3731a0c77ea06925b4309a4e9039c7b8b7ac2cb
aabssmc <lilmikeyninja203@gmail.com>
2023-12-30 17:28:50 -0800

    Added EffToggleAlerts

---
+++ src/main/java/lol/aabss/skuishy/hooks/vulcan/effects/EffToggleAlerts.java
@@ -0,0 +1,50 @@
+package lol.aabss.skuishy.hooks.vulcan.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import me.frep.vulcan.api.VulcanAPI;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+
+@Name("Vulcan - Toggle Alerts")
+@Description("Toggles alerts for a player.")
+@Examples({
+        "toggle alerts for player"
+})
+@Since("2.0")
+public class EffToggleAlerts extends Effect {
+
+    static{
+        Skript.registerEffect(EffToggleAlerts.class,
+                "toggle [the] [vulcan] [hack[ing]] alerts for %player%"
+        );
+    }
+
+    private Expression<Player> p;
+
+    @Override
+    protected void execute(@NotNull Event e) {
+        VulcanAPI.Factory.getApi().toggleAlerts(p.getSingle(e));
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "toggle alerts";
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        p = (Expression<Player>) exprs[0];
+        return true;
+    }
+}
