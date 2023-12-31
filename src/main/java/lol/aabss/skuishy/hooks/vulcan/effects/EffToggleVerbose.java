fc097329f550ffdd35d8e6ae70190c5a3b2986de
aabssmc <lilmikeyninja203@gmail.com>
2023-12-30 17:28:57 -0800

    Added EffToggleVerbose

---
+++ src/main/java/lol/aabss/skuishy/hooks/vulcan/effects/EffToggleVerbose.java
@@ -0,0 +1,50 @@
+package lol.aabss.skuishy.hooks.vulcan.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import me.frep.vulcan.api.VulcanAPI;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+
+@Name("Vulcan - Toggle Verbose")
+@Description("Toggles verbose for a player.")
+@Examples({
+        "toggle verbose for player"
+})
+@Since("2.0")
+public class EffToggleVerbose extends Effect {
+
+    static{
+        Skript.registerEffect(EffToggleVerbose.class,
+                "toggle [the] [vulcan] verbose for %player%"
+        );
+    }
+
+    private Expression<Player> p;
+
+    @Override
+    protected void execute(@NotNull Event e) {
+        VulcanAPI.Factory.getApi().toggleVerbose(p.getSingle(e));
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "toggle verbose";
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        p = (Expression<Player>) exprs[0];
+        return true;
+    }
+}
