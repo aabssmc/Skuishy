c5890ba9aab4a59ad8eceb7bbfe151eda9330078
aabssmc <lilmikeyninja203@gmail.com>
2024-01-02 13:46:00 -0800

    removed ExprNumber.java cuz it causes too many issues

--- src/main/java/lol/aabss/skuishy/events/CustomEvents.java
+++ src/main/java/lol/aabss/skuishy/events/CustomEvents.java
@@ -1,44 +1,30 @@
 package lol.aabss.skuishy.events;
 
+import io.papermc.paper.event.player.PlayerItemCooldownEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.Statistic;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerStatisticIncrementEvent;
-import org.bukkit.inventory.PlayerInventory;
 
 public class CustomEvents implements Listener {
 
     @EventHandler
-    public void onShieldBreak(PlayerStatisticIncrementEvent e) {
-        if (e.getStatistic() == Statistic.DAMAGE_BLOCKED_BY_SHIELD) {
-            PlayerInventory i = e.getPlayer().getInventory();
-            Material o = i.getItemInOffHand().getType();
-            Material m = i.getItemInMainHand().getType();
-            if (m == Material.SHIELD) {
-                if (e.getPlayer().getCooldown(m) > 0){
-                    Bukkit.getServer().getPluginManager().callEvent(new ShieldBreakEvent(e.getPlayer()));
-                }
-            } else if (o == Material.SHIELD) {
-                if (e.getPlayer().getCooldown(o) > 0){
-                    Bukkit.getServer().getPluginManager().callEvent(new ShieldBreakEvent(e.getPlayer()));
-                }
+    public void onShieldBreak(PlayerItemCooldownEvent e) {
+        if (e.getType() == Material.SHIELD) {
+            if (e.getCooldown() > 0){
+                new ShieldBreakEvent(e.getPlayer()).callEvent();
             }
         }
     }
 
     @EventHandler
     public void onHeadRotate(PlayerMoveEvent e) {
-        if (e.getFrom().getX() == e.getTo().getX()){
-            if (e.getFrom().getY() == e.getTo().getY()){
-                if (e.getFrom().getZ() == e.getTo().getZ()){
-                    if (e.getFrom().getYaw() != e.getTo().getYaw() || e.getFrom().getPitch() != e.getTo().getPitch()){
-                        Bukkit.getServer().getPluginManager().callEvent(new HeadRotationEvent(e.getPlayer(), e.getFrom(), e.getTo()));
-                    }
-                }
+        if (e.getFrom().getX() == e.getTo().getX() && e.getFrom().getY() == e.getTo().getY() && e.getFrom().getZ() == e.getTo().getZ()){
+            if (e.getFrom().getYaw() != e.getTo().getYaw() || e.getFrom().getPitch() != e.getTo().getPitch()){
+                Bukkit.getServer().getPluginManager().callEvent(new HeadRotationEvent(e.getPlayer(), e.getFrom(), e.getTo()));
             }
         }
+
     }
 }
