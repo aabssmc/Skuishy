81d1477fe542b4001087360d2a9ef2ec1d571545
aabssmc <lilmikeyninja203@gmail.com>
2024-01-31 11:39:33 -0800

    removed EvtHeadRotation.java (now in skript)

--- src/main/java/lol/aabss/skuishy/events/CustomEvents.java
+++ src/main/java/lol/aabss/skuishy/events/CustomEvents.java
@@ -1,11 +1,9 @@
 package lol.aabss.skuishy.events;
 
 import io.papermc.paper.event.player.PlayerItemCooldownEvent;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
 
 public class CustomEvents implements Listener {
 
@@ -16,15 +14,5 @@ public class CustomEvents implements Listener {
                 new ShieldBreakEvent(e.getPlayer()).callEvent();
             }
         }
-    }
-
-    @EventHandler
-    public void onHeadRotate(PlayerMoveEvent e) {
-        if (e.getFrom().getX() == e.getTo().getX() && e.getFrom().getY() == e.getTo().getY() && e.getFrom().getZ() == e.getTo().getZ()){
-            if (e.getFrom().getYaw() != e.getTo().getYaw() || e.getFrom().getPitch() != e.getTo().getPitch()){
-                Bukkit.getServer().getPluginManager().callEvent(new HeadRotationEvent(e.getPlayer(), e.getFrom(), e.getTo()));
-            }
-        }
-
     }
 }
