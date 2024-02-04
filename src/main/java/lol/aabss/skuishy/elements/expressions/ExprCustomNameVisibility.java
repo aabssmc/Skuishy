2d1909eb90fb0f8b9ba90debc5cb92dba3a2df86
aabssmc <lilmikeyninja203@gmail.com>
2024-02-04 12:05:27 -0800

    Fixed ExprCustomNameVisibility

--- src/main/java/lol/aabss/skuishy/elements/expressions/ExprCustomNameVisibility.java
+++ src/main/java/lol/aabss/skuishy/elements/expressions/ExprCustomNameVisibility.java
@@ -12,9 +12,11 @@ import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
 import org.jetbrains.annotations.NotNull;
 
-import org.eclipse.jdt.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("NullableProblems")
 @Name("Entity - Custom Name Visibility")
@@ -34,17 +36,18 @@ public class ExprCustomNameVisibility extends PropertyExpression<Entity, Boolean
 
     @Override
     protected @Nullable Boolean[] get(@NotNull Event e, Entity @NotNull [] source) {
-        Entity en = getExpr().getSingle(e);
-        if (en != null) return new Boolean[]{en.isCustomNameVisible()};
-        return new Boolean[]{null};
+        List<Boolean> entities = new ArrayList<>();
+        for (Entity entity : getExpr().getArray(e))
+            entities.add(entity.isCustomNameVisible());
+        return entities.toArray(Boolean[]::new);
     }
 
     @Override
-    public void change(@NotNull Event e, Object @NotNull [] delta, Changer.@NotNull ChangeMode mode){
-        if (mode == Changer.ChangeMode.SET) {
-            Entity en = getExpr().getSingle(e);
-            if (en != null) {
-                en.setCustomNameVisible((Boolean) delta[0]);
+    public void change(@NotNull Event e, Object @Nullable [] delta, Changer.@NotNull ChangeMode mode){
+        if (mode == Changer.ChangeMode.SET && delta != null) {
+            Entity[] en = getExpr().getArray(e);
+            for (Entity entity : en) {
+                entity.setCustomNameVisible((Boolean) delta[0]);
             }
         }
     }
