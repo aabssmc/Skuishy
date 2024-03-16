335f4db1808240cd4b7239b13096ce976e18ac8e
aabssmc <lilmikeyninja203@gmail.com>
2024-03-16 00:29:04 -0700

    Add ExprHologramName

---
+++ src/main/java/lol/aabss/skuishy/elements/decentholograms/expressions/ExprHologramName.java
@@ -0,0 +1,53 @@
+package lol.aabss.skuishy.elements.decentholograms.expressions;
+
+import ch.njol.skript.doc.*;
+import ch.njol.skript.expressions.base.PropertyExpression;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import eu.decentsoftware.holograms.api.holograms.Hologram;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Name("Decent Holograms - Hologram Name")
+@Description("Gets a hologram name of a hologram.")
+@Examples({
+        "send hologram name of {_holo}"
+})
+@Since("2.5")
+@RequiredPlugins("DecentHolograms")
+public class ExprHologramName extends PropertyExpression<Hologram, String> {
+
+    static {
+        register(ExprHologramName.class, String.class, "holo[gram] name", "holograms");
+    }
+
+    @Override
+    protected String @NotNull [] get(@NotNull Event event, Hologram[] source) {
+        List<String> names = new ArrayList<>();
+        for (Hologram holo : source){
+            names.add(holo.getName());
+        }
+        return names.toArray(String[]::new);
+    }
+
+    @Override
+    public @NotNull Class<? extends String> getReturnType() {
+        return String.class;
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "name of hologram";
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        setExpr((Expression<? extends Hologram>) exprs[0]);
+        return true;
+    }
+}
