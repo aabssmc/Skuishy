a8502f52f38835f6f770227d20d8b4a53c6d40ab
aabssmc <lilmikeyninja203@gmail.com>
2024-03-30 12:14:46 -0700

    Added ExprAllHolograms

---
+++ src/main/java/lol/aabss/skuishy/elements/decentholograms/expressions/ExprAllHolograms.java
@@ -0,0 +1,56 @@
+package lol.aabss.skuishy.elements.decentholograms.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.*;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import eu.decentsoftware.holograms.api.holograms.Hologram;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+import org.jetbrains.annotations.NotNull;
+
+import static eu.decentsoftware.holograms.api.commands.TabCompleteHandler.PLUGIN;
+
+@Name("Decent Holograms - All Holograms")
+@Description("Gets all the decent holograms' holograms.")
+@Examples({
+        "send all holograms"
+})
+@Since("2.6")
+@RequiredPlugins("DecentHolograms")
+public class ExprAllHolograms extends SimpleExpression<Hologram> {
+
+    static {
+        Skript.registerExpression(ExprAllHolograms.class, Hologram.class, ExpressionType.COMBINED,
+                "[all [of the]] [decent holograms|dh] holograms"
+        );
+    }
+
+    @Override
+    protected @Nullable Hologram @NotNull [] get(@NotNull Event e) {
+        return PLUGIN.getHologramManager().getHolograms().toArray(Hologram[]::new);
+    }
+
+    @Override
+    public boolean isSingle() {
+        return false;
+    }
+
+    @Override
+    public @NotNull Class<? extends Hologram> getReturnType() {
+        return Hologram.class;
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "all holograms";
+    }
+
+    @Override
+    public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        return true;
+    }
+}
