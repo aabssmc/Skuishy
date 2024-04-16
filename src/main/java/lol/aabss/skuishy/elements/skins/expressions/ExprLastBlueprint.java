bc7b8ec0f5e43f5c90ae81f0d15370e6e30b4991
aabssmc <lilmikeyninja203@gmail.com>
2024-04-15 23:50:13 -0700

    Added ExprLastBlueprint

---
+++ src/main/java/lol/aabss/skuishy/elements/skins/expressions/ExprLastBlueprint.java
@@ -0,0 +1,58 @@
+package lol.aabss.skuishy.elements.skins.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import lol.aabss.skuishy.other.blueprints.Blueprint;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+import org.jetbrains.annotations.NotNull;
+
+import static lol.aabss.skuishy.Skuishy.last_blueprint;
+
+@Name("Blueprint - Last Blueprint")
+@Description("Gets the last made blueprint.")
+@Examples({
+        "set {_blueprint} to last made blueprint"
+})
+@Since("2.6")
+public class ExprLastBlueprint extends SimpleExpression<Blueprint> {
+
+    static {
+        Skript.registerExpression(ExprLastBlueprint.class, Blueprint.class, ExpressionType.SIMPLE,
+                "[the] last [made|created] blueprint"
+        );
+    }
+
+    @Override
+    protected @Nullable Blueprint[] get(Event e) {
+        return new Blueprint[]{last_blueprint};
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public @NotNull Class<? extends Blueprint> getReturnType() {
+        return Blueprint.class;
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "last blueprint";
+    }
+
+    @Override
+    public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        return true;
+    }
+}
